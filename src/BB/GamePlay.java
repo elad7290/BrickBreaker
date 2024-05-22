b091026261a0ad542fb34dcb0bb7139385cdd073
elad7290 <elad7290@gmail.com>
2024-05-22 14:59:38 +0300

    אז יש לנו כדור שזז עך המסך ופדאל שגם זז וכאשר הכדור פוגע בפדאל הוא קופץ בחזרה. זה נכון גם כאשר הכדור פוגע בקצוות המשחק

--- src/BB/GamePlay.java
+++ src/BB/GamePlay.java
@@ -1,4 +1,115 @@
 package BB;
 
-public class GamePlay {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class GamePlay extends JPanel implements KeyListener, ActionListener {
+
+    private boolean play = false;
+    private int score = 0;
+    private int totalBricks =21;
+    private Timer timer;
+    private int delay = 8;
+    private int playerX = 310;
+    private int ballposX =120;
+    private int ballposY =350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+    public GamePlay(){
+        addKeyListener(this);
+        setFocusable(true);
+        setFocusTraversalKeysEnabled(false);
+        timer = new Timer(this.delay,this);
+        timer.start();
+    }
+    public void paint(Graphics graphics){
+        //the border of the game
+        graphics.setColor(Color.white);
+        graphics.fillRect(1,1,692,592);
+
+        graphics.setColor(Color.blue);
+        graphics.fillRect(0,0,3,592);
+        graphics.fillRect(0,0,692,3);
+        graphics.fillRect(691,0,3,592);
+        //the paddle
+        graphics.setColor(Color.orange);
+        graphics.fillRect(playerX,550,100,8);
+        //the ball
+        graphics.setColor(Color.BLACK);
+        graphics.fillOval(ballposX,ballposY,20,20);
+
+        graphics.dispose();
+    }
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        timer.start();
+        if(play){
+            //Ball - pedal interaction
+            if(new Rectangle(ballposX,ballposY,20,30).intersects(new Rectangle(playerX,550,100,8))){
+                ballYdir = -ballYdir;
+            }
+            ballposX+=ballXdir;
+            ballposY+=ballYdir;
+
+            if(ballposX<0){
+                ballXdir = -ballXdir;
+            }
+
+            if(ballposY<0){
+                ballYdir = -ballYdir;
+            }
+            if(ballposX>670){
+                ballXdir = -ballXdir;
+            }
+
+        }
+        repaint();
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+            if(playerX>=600){
+                playerX =600;
+            }else {
+                moveRight();
+            }
+        }
+        if(e.getKeyCode() == KeyEvent.VK_LEFT){
+            if(playerX<10){
+                playerX =10;
+            }else {
+                moveLeft();
+            }
+        }
+
+    }
+
+    private void moveLeft() {
+        play = true;
+        playerX-=40;
+    }
+
+    private void moveRight() {
+        play = true;
+        playerX+=40;
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
--- src/BB/Main.java
+++ src/BB/Main.java
@@ -6,10 +6,11 @@ public class Main {
     public static void main(String[] args) {
         JFrame obj = new JFrame();
         GamePlay gamePlay = new GamePlay();
-        obj.setBounds(10,10,700,600);
+        obj.setBounds(10,10,710,650);
         obj.setTitle("Brick Breaker");
         obj.setResizable(false);
         obj.setVisible(true);
         obj.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        obj.add(gamePlay);
     }
 }
